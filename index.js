45c3299b736d822a456af6fc16d50caf254ba0c5
faoberilo <89050385+faoberilo@users.noreply.github.com>
2021-11-22 21:31:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express =  require('express');
 const cors = require('cors');
 const Conn = require('./conn/conn');
@@ -12,8 +16,13 @@ app.use(cors());
 app.options("*", cors()); 
 app.use('/tarefas', tarefasRouter);
 
+const db_url=process.env.DB_URL;
+const db_user=process.env.DB_USER;
+const db_pass=process.env.DB_PASS;
+const db_data=process.env.DB_DATA;
 
-Conn();
-app.listen(port, ()=>{
+Conn(db_url, db_user, db_pass, db_data);
+
+app.listen(process.env.PORT || port, ()=>{
     console.log(`Servidor rodando no http://localhost/${port}`);
 })
